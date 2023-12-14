bd297b29e6d3f81e260daae50f86006834dcb732
Leonid Fiksler <reddiridabl666@gmail.com>
2023-12-14 18:10:36 +0300

    Fixed constants and asserts

--- hw/code/test_ad_groups.py
+++ hw/code/test_ad_groups.py
@@ -1,4 +1,3 @@
-import time
 from base import BaseCase
 from ui.pages.ad_groups_page import AdGroupCreationPage
 from ui.fixtures import ad_groups_page, ad_group_creation_page, credentials
@@ -7,6 +6,20 @@ import pytest
 
 
 class TestAdGroups(BaseCase):
+    DEMOGRAPHY_MAX_AGE = 70
+    DEMOGRAPHY_MIN_AGE = 14
+    PLACEMENT_OPTIONS = ['ВКонтакте', 'Одноклассники',
+                         'Проекты VK', 'Рекламная сеть']
+
+    LIST_REGION_INPUT = ['Россия', 'Москва', '468']
+    LIST_REGIONS_OUTPUT = ['Москва',
+                           'Республика Крым',
+                           'Россия']
+
+    REGIONS = ['Россия', 'Европа']
+
+    REGIONS_ADDED = 'Добавлены 3 региона'
+
     @pytest.mark.parametrize('region', ('Россия',))
     def test_select_region(self, ad_group_creation_page, region):
         ad_group_creation_page.search_regions(region)
@@ -15,12 +28,11 @@ class TestAdGroups(BaseCase):
         assert ad_group_creation_page.selected_regions() == [region]
 
     def test_clear_regions_selection(self, ad_group_creation_page):
-        regions = ['Россия', 'Европа']
-        for region in regions:
+        for region in self.REGIONS:
             ad_group_creation_page.search_regions(region)
             ad_group_creation_page.select_region(region)
 
-        assert ad_group_creation_page.selected_regions() == regions
+        assert ad_group_creation_page.selected_regions() == self.REGIONS
 
         ad_group_creation_page.clear_region_selection()
         assert ad_group_creation_page.selected_regions() == []
@@ -33,28 +45,24 @@ class TestAdGroups(BaseCase):
         assert any('Новороссийск' in elem for elem in shown)
 
     def test_remove_region_from_selection(self, ad_group_creation_page):
-        regions = ['Россия', 'Европа']
-        for region in regions:
+        for region in self.REGIONS:
             ad_group_creation_page.search_regions(region)
             ad_group_creation_page.select_region(region)
 
-        assert ad_group_creation_page.selected_regions() == regions
+        assert ad_group_creation_page.selected_regions() == self.REGIONS
 
-        ad_group_creation_page.remove_region_from_selection('Европа')
-        assert ad_group_creation_page.selected_regions() == ['Россия']
+        ad_group_creation_page.remove_region_from_selection(self.REGIONS[1])
+        assert ad_group_creation_page.selected_regions() == [self.REGIONS[0]]
 
     def test_add_regions_by_list(self, ad_group_creation_page):
-        regions = ['Россия', 'Москва', '468']
-        ad_group_creation_page.add_by_list(regions)
+        ad_group_creation_page.add_by_list(self.LIST_REGION_INPUT)
 
-        assert ad_group_creation_page.add_by_list_status() == 'Добавлены 3 региона'
+        assert ad_group_creation_page.add_by_list_status() == self.REGIONS_ADDED
         ad_group_creation_page.close_list_add_modal()
 
         selected_regions = ad_group_creation_page.selected_regions()
-
-        assert 'Москва' in selected_regions
-        assert 'Республика Крым' in selected_regions
-        assert 'Россия' in selected_regions
+        for region in self.LIST_REGIONS_OUTPUT:
+            assert region in selected_regions
 
     def test_must_choose_at_least_one_device(self, ad_group_creation_page):
         ad_group_creation_page.toggle_devices_section()
@@ -70,33 +78,31 @@ class TestAdGroups(BaseCase):
         ad_group_creation_page.show_placement_options()
         placement_options = ad_group_creation_page.get_placement_options()
 
-        assert 'ВКонтакте' in placement_options
-        assert 'Одноклассники' in placement_options
-        assert 'Проекты VK' in placement_options
-        assert 'Рекламная сеть' in placement_options
+        for option in self.PLACEMENT_OPTIONS:
+            assert option in placement_options
 
     def test_min_age_less_than_max(self, ad_group_creation_page):
         ad_group_creation_page.toggle_demography_section()
-        ad_group_creation_page.select_max_age(70)
+        ad_group_creation_page.select_max_age(self.DEMOGRAPHY_MAX_AGE)
 
         assert all(
-            elem <= 70 for elem in ad_group_creation_page.available_min_age())
+            elem <= self.DEMOGRAPHY_MAX_AGE for elem in ad_group_creation_page.available_min_age())
 
     def test_max_age_greater_than_min(self, ad_group_creation_page):
         ad_group_creation_page.toggle_demography_section()
-        ad_group_creation_page.select_min_age(14)
+        ad_group_creation_page.select_min_age(self.DEMOGRAPHY_MIN_AGE)
 
         assert all(
-            elem >= 14 for elem in ad_group_creation_page.available_max_age())
+            elem >= self.DEMOGRAPHY_MIN_AGE for elem in ad_group_creation_page.available_max_age())
 
     def test_audience_negative_search_toggle(self, ad_group_creation_page):
         ad_group_creation_page.toggle_audience_section()
 
         ad_group_creation_page.show_negative_audience_search()
-        ad_group_creation_page.is_negative_audience_search_shown()
+        assert ad_group_creation_page.is_negative_audience_search_shown()
 
         ad_group_creation_page.hide_negative_audience_search()
-        ad_group_creation_page.is_negative_audience_toggle_shown()
+        assert ad_group_creation_page.is_negative_audience_toggle_shown()
 
     def test_select_audience(self, keyword_audience, ad_group_creation_page):
         ad_group_creation_page.toggle_audience_section()
@@ -121,6 +127,7 @@ class TestAdGroups(BaseCase):
         ids = ad_group_drafts_page.shown_ad_group_ids()
         ad_group_drafts_page.edit_ad_group_draft(ids[0])
         ad_group_drafts_page.check_url(AdGroupCreationPage.url)
+        assert AdGroupCreationPage.url in ad_group_drafts_page.driver.url
 
     def test_select_ad_group(self, ad_group_drafts_page):
         ids = ad_group_drafts_page.shown_ad_group_ids()
--- hw/code/test_audience.py
+++ hw/code/test_audience.py
@@ -28,6 +28,9 @@ class TestAudience(BaseCase):
     keywords_name = 'Аудитория с ключевыми словами'
     based_on_other_name = 'Аудитория на основе старой'
 
+    RULE_AND = 'и'
+    RULE_OR = 'или'
+
     def test_long_audience_name(self, audience_page):
         audience_page.set_audience_name('a'*256)
         audience_page.has_long_name_error()
@@ -55,14 +58,18 @@ class TestAudience(BaseCase):
     def test_boolean_rule(self, audience_page):
         audience_page.set_audience_name(self.keywords_name)
 
-        audience_page.add_source(data=keywords_payload(name='Условие 1'))
-        audience_page.add_source(data=keywords_payload(name='Условие 2'))
+        first_source_name = 'Условие 1'
+        second_source_name = 'Условие 1'
 
-        audience_page.set_rule('и')
-        assert audience_page.get_rule() == 'и'
+        audience_page.add_source(data=keywords_payload(name=first_source_name))
+        audience_page.add_source(
+            data=keywords_payload(name=second_source_name))
 
-        audience_page.set_rule('или')
-        assert audience_page.get_rule() == 'или'
+        audience_page.set_rule(self.RULE_AND)
+        assert audience_page.get_rule() == self.RULE_AND
+
+        audience_page.set_rule(self.RULE_OR)
+        assert audience_page.get_rule() == self.RULE_OR
 
     def test_create_from_other(self, keyword_audience, audience_based_on_other):
         pass
